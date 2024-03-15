93c74bcb2492e46c011889b119d9eac979ac2ff3
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2024-03-15 20:06:32 -0300

    removido import náo utilizados

--- src/main/java/br/com/pagamentovalhallakitchen/adapter/driver/form/RespostaPagamentoForm.java
+++ src/main/java/br/com/pagamentovalhallakitchen/adapter/driver/form/RespostaPagamentoForm.java
@@ -1,9 +1,7 @@
 package br.com.pagamentovalhallakitchen.adapter.driver.form;
 
-import br.com.pagamentovalhallakitchen.core.domain.Status;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 @Builder
