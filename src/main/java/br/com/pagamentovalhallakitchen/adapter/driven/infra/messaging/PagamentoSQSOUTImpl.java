e5d1a588feedffcf8a6cf34b47ac8bb2c845b4f6
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2024-03-18 18:39:32 -0300

    FIX: correcao para utilizar variavel correta. Ajustado nome da classe

--- src/main/java/br/com/pagamentovalhallakitchen/adapter/driven/infra/messaging/PagamentoSQSOUTImpl.java
+++ src/main/java/br/com/pagamentovalhallakitchen/adapter/driven/infra/messaging/PagamentoSQSOUTImpl.java
@@ -1,24 +1,26 @@
 package br.com.pagamentovalhallakitchen.adapter.driven.infra.messaging;
 
-import br.com.pagamentovalhallakitchen.core.applications.ports.PagamentoSQSOUTAdapter;
+import br.com.pagamentovalhallakitchen.core.applications.ports.PagamentoSQSOUT;
 import br.com.pagamentovalhallakitchen.core.domain.Pagamento;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PagamentoSQSOUT implements PagamentoSQSOUTAdapter {
+public class PagamentoSQSOUTImpl implements PagamentoSQSOUT {
 
-    @Autowired
-    private SqsTemplate SqsTemplate;
+    private SqsTemplate sqsTemplate;
 
     @Value("${queue.retorno-pagamento}")
     private String queueConfirmarPagamento;
 
+    public PagamentoSQSOUTImpl (SqsTemplate sqsTemplate) {
+        this.sqsTemplate = sqsTemplate;
+    }
+
     @Override
     public void publicarRetornoPagamento(Pagamento pagamento) {
-        SqsTemplate
+        this.sqsTemplate
                 .send(sqsSendOptions ->
                         sqsSendOptions
                                 .queue(queueConfirmarPagamento)
