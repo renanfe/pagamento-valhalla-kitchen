ded0d7956039f2ff51eed5fed56520d3833ae8dd
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2024-03-18 18:36:18 -0300

    Alterado nome da classe

--- src/main/java/br/com/pagamentovalhallakitchen/adapter/driven/infra/ports/PagamentoService.java
+++ src/main/java/br/com/pagamentovalhallakitchen/adapter/driven/infra/ports/PagamentoService.java
@@ -1,10 +1,9 @@
 package br.com.pagamentovalhallakitchen.adapter.driven.infra.ports;
 
 import br.com.pagamentovalhallakitchen.adapter.driver.form.PagamentoForm;
-import br.com.pagamentovalhallakitchen.adapter.driver.form.RespostaPagamentoForm;
 import br.com.pagamentovalhallakitchen.core.domain.Pagamento;
 
-public interface PagamentoServiceAdapter {
+public interface PagamentoService {
 
     public Pagamento criarPagamento(PagamentoForm pagamentoForm);
 
