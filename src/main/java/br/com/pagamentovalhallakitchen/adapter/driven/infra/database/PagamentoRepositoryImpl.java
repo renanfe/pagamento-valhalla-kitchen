032dae41971e7f1d44cd89ce10c33aca0b89ccff
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2024-03-18 18:35:45 -0300

    Alterado nome da classe e variavel interface

--- src/main/java/br/com/pagamentovalhallakitchen/adapter/driven/infra/database/PagamentoRepositoryImpl.java
+++ src/main/java/br/com/pagamentovalhallakitchen/adapter/driven/infra/database/PagamentoRepositoryImpl.java
@@ -3,7 +3,7 @@ package br.com.pagamentovalhallakitchen.adapter.driven.infra.database;
 import br.com.pagamentovalhallakitchen.adapter.driven.infra.jpa.PagamentoRepositoryJpa;
 import br.com.pagamentovalhallakitchen.adapter.driven.infra.entity.PagamentoEntity;
 import br.com.pagamentovalhallakitchen.adapter.utils.mappers.PagamentoMapper;
-import br.com.pagamentovalhallakitchen.core.applications.ports.PagamentoRepositoryAdapter;
+import br.com.pagamentovalhallakitchen.core.applications.ports.PagamentoRepository;
 import br.com.pagamentovalhallakitchen.core.domain.Pagamento;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,11 +12,11 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public class PagamentoRepository implements PagamentoRepositoryAdapter {
+public class PagamentoRepositoryImpl implements PagamentoRepository {
 
     private final PagamentoRepositoryJpa pagamentoRepositoryJpa;
 
-    public PagamentoRepository (PagamentoRepositoryJpa pagamentoRepositoryJpa) {
+    public PagamentoRepositoryImpl (PagamentoRepositoryJpa pagamentoRepositoryJpa) {
         this.pagamentoRepositoryJpa = pagamentoRepositoryJpa;
     }
 
