fae5488ea2c88044d0171ceb82c56ae841eca027
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2024-03-17 21:49:18 -0300

    Alterado nome interface para o Adapter

--- src/main/java/br/com/pagamentovalhallakitchen/core/applications/ports/PagamentoRepositoryAdapter.java
+++ src/main/java/br/com/pagamentovalhallakitchen/core/applications/ports/PagamentoRepositoryAdapter.java
@@ -5,7 +5,7 @@ import br.com.pagamentovalhallakitchen.core.domain.Pagamento;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface PagamentoRepository {
+public interface PagamentoRepositoryAdapter {
 
     Optional<Pagamento> buscarPagamento(Long id);
     Pagamento salvarPagamento(Pagamento pagamento);
